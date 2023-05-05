bd2782a6d20be676148a304a0ea7c32d58c4cb2a
VibhorR <vibhorrawat95@gmail.com>
2023-05-05 14:11:37 +0530

    Js logic
    Added functionality to buttons

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
+const addItem = document.getElementById('plus');
+
+const removeItem = document.getElementById('minus');
+
+function  addToCart(){
+    let output = document.getElementById('text');
+    let result = Number(output.innerText) + 1 
+        output.innerText = result;
+        
+}
+
+function removeFromCart(){
+    let output = document.getElementById('text');
+    let result = Number(output.innerText) - 1
+        output.innerText = result;
+    
+        if (result < 0 ){
+            output.innerText = 0
+            alert("Item cannot be in negative value ")
+            
+        }
+}
 
