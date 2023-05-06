9173b9bb196b6eafc896980fd0fcca6e98b87635
VibhorR <vibhorrawat95@gmail.com>
2023-05-06 14:33:37 +0530

    modal
    images on when clicked can be displayed inside modal

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-const addItem = document.getElementById('plus');
-
-const removeItem = document.getElementById('minus');
-
-function  addToCart(){
+function  addSelection(){
     let output = document.getElementById('text');
     let result = Number(output.innerText) + 1 
         output.innerText = result;
         
 }
 
-function removeFromCart(){
+function removeSelection(){
     let output = document.getElementById('text');
     let result = Number(output.innerText) - 1
         output.innerText = result;
@@ -21,3 +17,23 @@ function removeFromCart(){
         }
 }
 
+//function to openNodal
+function openModal(image) {
+    var modal = document.getElementById("myModal");
+    var modalImg = document.getElementById("modalImg");
+    var btn = document.getElementById("myBtn");
+    modal.style.display = "block";
+    modalImg.src = image.src;
+
+    // Hide the modal when the close button is clicked
+    btn.onclick = function() {
+        modal.style.display = "none";
+      }
+
+    // Hide the modal when clicking outside the modal content
+    window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+}
